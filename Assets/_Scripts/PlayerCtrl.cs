0ad99e3a2590ca14ec9948c1e1a9660f3c442651
Kim JeongHo <cpls9320@gmail.com>
2024-07-11 14:40:14 +0900

    player 이동시 회전까지

--- Assets/_Scripts/PlayerCtrl.cs
+++ Assets/_Scripts/PlayerCtrl.cs
@@ -8,32 +8,36 @@ public class PlayerCtrl : MonoBehaviour
     float inputH = 0.0f;
     float inputV = 0.0f;
     Vector3 moveDir = Vector3.zero;
-    float moveSpeed = 5.0f;
+    float moveSpeed = 3.0f;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
     }
 
-    void Start()
-    {
-        
-    }
+    //void Start() { }
 
     void FixedUpdate()
     {
+        Move();
+    }
+
+    //void Update() { }
+
+    private void Move()
+    {
         inputH = Input.GetAxis("Horizontal");
         inputV = Input.GetAxis("Vertical");
+
         moveDir.x = inputH;
+        moveDir.y = 0.0f;
         moveDir.z = inputV;
         moveDir.Normalize();
 
         Vector3 pos = transform.position + moveDir * moveSpeed * Time.fixedDeltaTime;
         rigid.MovePosition(pos);
+        rigid.MoveRotation(Quaternion.LookRotation(moveDir));
     }
 
-    void Update()
-    {
-        
-    }
+
 }
